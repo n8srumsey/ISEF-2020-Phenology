08c13ed497b845b9bf617fffbc5912e694ae8b3d
n8srumsey <nathan.s.rumsey@gmail.com>
2021-01-19 12:30:58 -0800

    Update result saving functionality

--- Phenophase_Classification/train.py
+++ Phenophase_Classification/train.py
@@ -1,21 +1,21 @@
-import os
-import math
 import json
-import uuid
-import numpy as np
+import math
+import os
+import re
 
+import numpy as np
 from skimage.io import imread
 from sklearn.utils import shuffle
-
-from keras import Sequential
-from keras.layers import BatchNormalization, Conv2D, Dense, Dropout, Flatten, MaxPool2D
-from keras.losses import BinaryCrossentropy
-from keras.metrics import AUC, BinaryAccuracy, Precision, Recall
-from keras.utils import Sequence
-from keras.callbacks import History, EarlyStopping
+from tensorflow.keras import Sequential
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.layers import (BatchNormalization, Conv2D, Dense,
+                                     Dropout, Flatten, MaxPool2D)
+from tensorflow.keras.losses import BinaryCrossentropy
+from tensorflow.keras.metrics import AUC, BinaryAccuracy, Precision, Recall
+from tensorflow.keras.regularizers import l1_l2, l2
+from tensorflow.keras.utils import Sequence
 
 from data_preprocessing import image_shape
-
 
 K = 10
 DATA_DIR = './Phenophase_Classification/data/'
@@ -25,8 +25,8 @@ RESULTS_DIR = './Phenophase_Classification/results/'
 
 # define the data subsets
 all_images = np.array(os.listdir(IMAGES_DIR))
-all_images = all_images[:-(len(all_images) % K)]
-
+if len(all_images) % K != 0:
+    all_images = all_images[:-(len(all_images) % K)]
 data_partitions = np.split(all_images, K)
 
 
@@ -72,50 +72,53 @@ def get_generators(num_fold, batch_size, shuffle=False):
 
 
 # define model
-def define_model():
+def define_model(k):
     model = Sequential([
-        Conv2D(filters=16, kernel_size=(3, 3), strides=(2, 2), activation='relu', input_shape=image_shape),
+        Conv2D(filters=16, kernel_size=(3, 3), strides=(2, 2), activation='relu', use_bias=True, input_shape=image_shape, kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4),
+               bias_regularizer=l2(1e-4), activity_regularizer=l2(1e-5)),
         BatchNormalization(),
         MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
-        Conv2D(filters=8, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding="same"),
+        Conv2D(filters=8, kernel_size=(3, 3), strides=(1, 1), activation='relu', padding='same', use_bias=True, kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4), 
+               bias_regularizer=l2(1e-4), activity_regularizer=l2(1e-5)),
         BatchNormalization(),
         MaxPool2D(pool_size=(3, 3), strides=(2, 2)),
         Flatten(),
-        Dense(128, activation='relu'),
+        Dense(128, activation='relu', use_bias=True, kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4), bias_regularizer=l2(1e-4), activity_regularizer=l2(1e-5)),
         Dropout(0.5),
-        Dense(64, activation='relu'),
+        Dense(64, activation='relu', use_bias=True, kernel_regularizer=l1_l2(l1=1e-5, l2=1e-4), bias_regularizer=l2(1e-4), activity_regularizer=l2(1e-5)),
         Dense(2, activation='sigmoid')
     ])
-    model_summary = model.summary()
+    if k == None:
+        return model
     model.compile(optimizer='adam', loss=BinaryCrossentropy(), metrics=[BinaryAccuracy(threshold=0.5), AUC(), Precision(), Recall()])
-    return model, model_summary
+    return model
 
 
 # training parameters
 params = {
     # dataset and model architechture
+    'k': K,
     'batch_size': 256,
     'shuffle': True,
     'mode': 'binary',
 
     # training
-    'epochs': 3,
-
+    'epochs': 5,
 }
 
-callbacks = [History(),
-             EarlyStopping(monitor='val_binary_accuracy', min_delta=0.005, patience=3, mode='max', baseline=0.8),
-             ]
+callbacks = [EarlyStopping(monitor='val_loss', min_delta=0.005, patience=5, mode='min')]
 
+
+""" Start Training """
 if __name__ == '__main__':
     results = {}
 
     # k-fold cross validation loop
     for k in range(K):
+        print('\n\n\n*** STARTING ITERATION {} out of {} ***\n'.format(k+1, K))
         train_gen, val_gen = get_generators(k, batch_size=params['batch_size'], shuffle=params['shuffle'])
-        model, model_summary = define_model()
-        results['model_summary'] = model_summary
-
+        model = define_model(k)
+        
         # train
         history = model.fit(train_gen, validation_data=val_gen, epochs=params['epochs'], verbose=1, callbacks=callbacks)
 
@@ -123,15 +126,34 @@ if __name__ == '__main__':
         results['iteration_{}'.format(k)] = history.history
 
     # summarize results
-    summary = {'loss': [], 'binary_accuracy': [], 'auc': [], 'precision': [], 'recall': [], 'val_loss': [],
+    iteration_summaries = {'loss': [], 'binary_accuracy': [], 'auc': [], 'precision': [], 'recall': [], 'val_loss': [],
                'val_binary_accuracy': [], 'val_auc': [], 'val_precision': [], 'val_recall': []}
+
     for result in results.values():
         for metric in result.keys():
-            summary[metric].append(result[metric][-1])
+            metric_name = metric + ''
+            if re.search('_\d', metric): metric_name = re.sub('_\d', '', metric_name)
+            iteration_summaries[metric_name].append(result[metric][-1])
+    
+    results['iteration_summaries'] = iteration_summaries
+
+    summary = {'loss': [], 'binary_accuracy': [], 'auc': [], 'precision': [], 'recall': [], 'val_loss': [],
+               'val_binary_accuracy': [], 'val_auc': [], 'val_precision': [], 'val_recall': []}
+    for metric in results['iteration_summaries'].keys():
+        sum = 0
+        for entry in results['iteration_summaries'][metric]:
+            sum += entry
+        ln = float(len(results['iteration_summaries'][metric]))
+        summary[metric] = sum / ln
     results['summary'] = summary
+        
+    results['model_summary'] = []
+    define_model(None).summary(print_fn=lambda x: results['model_summary'].append(x))
+    
+    results['params'] = params
 
-    instance_uuid = uuid.uuid1()[:8]
-    results['uuid'] = instance_uuid
+    num_result = len(os.listdir(RESULTS_DIR))
+    save_to = RESULTS_DIR + 'result_{}.json'.format(num_result)
 
-    with open(RESULTS_DIR + instance_uuid + '_results.json', 'w') as file:
-        json.dumps(results, file, indent=4)
+    with open(save_to, 'w') as file:
+        json.dump(results, file, indent=4)
